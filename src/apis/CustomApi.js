9bc16cbd5624dc9bf976b608609ea6eb78dc9d17
SunYerim <yaelim6@pukyong.ac.kr>
2024-07-09 23:23:49 +0900

    fix: customapi

--- src/apis/CustomApi.js
+++ src/apis/CustomApi.js
@@ -9,7 +9,7 @@ const CustomApiLogin = axios.create({
 });
 
 const CustomApi = axios.create({
-  baseURL: "http://13.124.194.48:9000",
+  baseURL: "/api",
   headers: {
     'Content-Type': 'application/json',
   },
